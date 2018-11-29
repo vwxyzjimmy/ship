88d7c35f1c1239b6952e2795d439184a342b12e6
vwxyzjimmy <vwxyzjimmy@gmail.com>
2018-11-29 16:58:42 +0800

    add z-score

--- ship_test.py
+++ ship_test.py
@@ -7,8 +7,9 @@ key_word = sys.argv[1]
 
 filename = key_word + ".xls"
 book = xlwt.Workbook(encoding="utf-8")
-sheet1 = book.add_sheet("Sheet1")
-sheet2 = book.add_sheet("Sheet2")
+sheet1 = book.add_sheet("original-data")
+sheet2 = book.add_sheet("min-max")
+sheet3 = book.add_sheet("z-score")
 db_connect = pymysql.connect("140.116.39.173", "root", "rootroot", "shipping", charset="utf8")
 cursor = db_connect.cursor()
 mysql_show_db_command = 'show databases'
@@ -25,6 +26,7 @@ for i in range(len(temp)):
 	column_matrix.append(column)
 	sheet1.write(0, i, column)
 	sheet2.write(0, i, column)
+	sheet3.write(0, i, column)
 
 mysql_data_command = "select * from " + key_word
 cursor.execute(mysql_data_command)
@@ -52,6 +54,7 @@ for i in range(len(data)):
 			sheet1.write(i+1, j, data_matrix[i][j])
 inverse_matrix = map(list, zip(*float_matrix))
 
+#min-max normalization
 temp_max= []
 temp_min= []
 for i  in range(len(inverse_matrix)):
@@ -86,6 +89,67 @@ for i in range(len(inverse_matrix)):
 					sheet2.write(j+1, i, 'None')
 			except:
 				sheet2.write(j+1, i, (inverse_matrix[i][j]-temp_min[i])/temp_range)
+
+#z-score normalization
+mean_number = []
+valid_number = []
+mean_matrix = []
+for i in range(len(inverse_matrix)):
+	mean_number.append(0)
+	valid_number.append(0)
+	mean_matrix.append([])
+	valid_count = 0
+	add_total = 0
+	for j in range(len(inverse_matrix[i])):
+		if inverse_matrix[i][j] != -999:
+			add_total = add_total + inverse_matrix[i][j]
+			valid_number[i] = valid_number[i] + 1
+		else:
+			pass
+	try:
+		mean = add_total/valid_number[i]
+	except:
+		mean = 0
+	mean_number[i] = mean
+
+without_None_matrix = []
+for i in range(len(inverse_matrix)):
+	without_None_matrix.append([])
+	test = 0
+	for j in range(len(inverse_matrix[i])):
+		if inverse_matrix[i][j] != -999:
+			without_None_matrix[i].append(inverse_matrix[i][j])
+		else:
+			pass
+
+std_number = []
+for i in range(len(without_None_matrix)):
+	a = np.array(without_None_matrix[i])
+	std_number.append(np.std(a))
+	if i == 0:
+		pass
+	else:
+		sheet3.write(1, i, mean_number[i])
+		sheet3.write(2, i, std_number[i])
+sheet3.write(1, 0, "mean")
+sheet3.write(2, 0, "std")
+
+std_matrix = []
+for i in range(len(inverse_matrix)):
+	std_matrix.append([])	
+	for j in range(len(inverse_matrix[i])):
+		if i == 0:
+			try:
+				std_matrix[i].append(int(inverse_matrix[i][j]))
+			except:
+				std_matrix[i].append(0)
+		else:
+			if inverse_matrix[i][j] != -999:
+				std_matrix[i].append((inverse_matrix[i][j]-mean_number[i])/std_number[i])
+			else:
+				std_matrix[i].append("None")
+		sheet3.write(j+3, i, std_matrix[i][j])
+
 book.save(filename)
 '''
 cursor.execute("select * from mfm order by `Serial` DESC limit 1 ")
